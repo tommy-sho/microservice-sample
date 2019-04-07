ad735a343475edc9f709e8bc56da0bd60226c9ba
tomiokashougo <tomiokasyogo@gmail.com>
2019-04-07 12:32:31 +0900

    create services

---
+++ pkg/gcd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net"
+	"log"
+
+	"github.com/tommy-sho/microservice-sample/pb"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct{}
+
+func (s *server) Compute(ctx context.Context, r *pb.GCDRequest) (*pb.GCDResponse, error) {
+	a, b := r.A, r.B
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return &pb.GCDResponse{Result: a}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGCDServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
